fb3a18194e690590b87b84de1fb5355910f16233
Akash RP <akashrp512@gmail.com>
2022-11-07 09:55:58 +0530

    Commit

--- src/SeasonDisplay.jsx
+++ src/SeasonDisplay.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from 'react';
 
-const SeasonDisplay = () => {
+
+const seasonConfig ={
+   summer:{},
+   winter :{}
+
+};
+
+const getSeason= (latitude, month) =>{
+ if(month>2 && month<9) {
+  return latitude > 0 ? 'Summer' :'winter';
+ } else {
+  return latitude > 0 ? 'winter' : 'summer';
+ }
+   
+};
+
+const SeasonDisplay = (props) => {
+  const season=getSeason(props.latitude, new Date().getMonth());
+  const text = season === 'winter' ? 'Burr, it is chilly' : 'Lets hit the beach';
+  const iconName = season === 'winter' ? 'snowflake' : 'sun';
+
   return (
     <div>
-      Season Display
+     <i className={`${iconName} icon`} />
+    <h1>{ text }</h1>
+    <i className={`${iconName} icon`} />
     </div>
   )
 }
